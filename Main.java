b7cf1d9d59c24aca6ddf3e22d693802393787fee
Fl0piX <richar_5@epita.fr>
2017-07-18 15:43:57 +0200

    type d'alignement greedy fait

--- Main.java
+++ Main.java
@@ -28,15 +28,10 @@ public class Main {
 //        System.out.println(test);
 
         //test alignAnnotations
-        float[][] corresp = alignAnnotations(allFile1, allFile2);
-        String a = "";
+        Annotation[] corresp = alignAnnotations(allFile1, allFile2, arg[2]);
         for (int i=0 ; i<corresp.length ; i++) {
-            for (int j=0 ; j<corresp[i].length ; j++) {
-                a += corresp[i][j] + " | ";
-            }
-            a += "\n";
+            System.out.println(corresp[i]);
         }
-        System.out.println(a);
 
         // test score
 //        System.out.println("Similarity score : "+score(allFile1, allFile2, arg[2]));
@@ -81,55 +76,101 @@ public class Main {
     // 3rd parameter :  weakprecision || weakrecall || weakF-measure ||
     //                  strictprecision || strictrecall || strictF-measure ||
     //                  weightedprecision || weightedrecall || weightedF-measure
-//    public static float score(Annotation[] th, Annotation[] tr, String typeScore) {
-//        Annotation[] correspTh = alignAnnotations(th, tr); // put in an annotations table the intersecting annotations which are of the same type from both Annotation tables
-//        float result = -1;
-//        int nbMatches = 0;
-//
-//        // adjust the matches number depending on the 3rd parameter
-//        for (int i=0 ; i<correspTh.length ; i++) {
-//            if (correspTh[i] != null) {
-//                if (typeScore.substring(0, 4).equals("weak")) {
-//                    nbMatches++;
-//                }
-//                if (typeScore.substring(0, 6).equals("strict")) {
-//                    if (correspTh[i].getStart() == th[i].getStart()
-//                            && correspTh[i].getEnd() == th[i].getEnd()) {
-//                        nbMatches++;
-//                    }
-//                }
-//                if (typeScore.substring(0, 8).equals("weighted")) {
-//                    nbMatches += correspTh[i].intersectionPercentage(th[i]);
-//                }
-//            }
-//        }
-//
-//        // choose the correct calculation depending on the 3rd parameter
-//        if (typeScore.substring(typeScore.length()-9, typeScore.length()).equals("precision")) {
-//            result = (float) nbMatches / th.length;
-//        }
-//        if (typeScore.substring(typeScore.length()-6, typeScore.length()).equals("recall")) {
-//            result = (float) nbMatches / tr.length;
-//        }
-//        if (typeScore.substring(typeScore.length()-9, typeScore.length()).equals("F-measure")) {
-//            result = (float) ( 2 * (nbMatches/th.length) * (nbMatches/tr.length) ) / ( (nbMatches/th.length) + (nbMatches/tr.length) );
-//        }
-//
-//        return result;
-//    }
+    public static float score(Annotation[] th, Annotation[] tr, String typeScore, String typeAlignment) {
+        Annotation[] correspTh = alignAnnotations(th, tr, typeAlignment); // put in an annotations table the intersecting annotations which are of the same type from both Annotation tables
+        float result = -1;
+        int nbMatches = 0;
 
-
-    // returns an Annotation table containing the intersecting annotations which are of the same type from both Annotation tables
-    public static float[][] alignAnnotations(Annotation[] th, Annotation[] tr) {
-        float[][] t = new float[th.length][tr.length];
-
-        for (int i=0 ; i<th.length ; i++) {
-            for (int j=0 ; j<tr.length ; j++) {
-                t[i][j] = th[i].intersectionPercentage(tr[j]);
+        // adjust the matches number depending on the 3rd parameter
+        for (int i=0 ; i<correspTh.length ; i++) {
+            if (correspTh[i] != null) {
+                if (typeScore.substring(0, 4).equals("weak")) {
+                    nbMatches++;
+                }
+                if (typeScore.substring(0, 6).equals("strict")) {
+                    if (correspTh[i].getStart() == th[i].getStart()
+                            && correspTh[i].getEnd() == th[i].getEnd()) {
+                        nbMatches++;
+                    }
+                }
+                if (typeScore.substring(0, 8).equals("weighted")) {
+                    nbMatches += correspTh[i].intersectionPercentage(th[i]);
+                }
             }
         }
 
-        return t;
+        // choose the correct calculation depending on the 3rd parameter
+        if (typeScore.substring(typeScore.length()-9, typeScore.length()).equals("precision")) {
+            result = (float) nbMatches / th.length;
+        }
+        if (typeScore.substring(typeScore.length()-6, typeScore.length()).equals("recall")) {
+            result = (float) nbMatches / tr.length;
+        }
+        if (typeScore.substring(typeScore.length()-9, typeScore.length()).equals("F-measure")) {
+            result = (float) ( 2 * (nbMatches/th.length) * (nbMatches/tr.length) ) / ( (nbMatches/th.length) + (nbMatches/tr.length) );
+        }
+
+        return result;
+    }
+
+
+    // returns an Annotation table containing the intersecting annotations which are of the same type from both Annotation tables
+    public static Annotation[] alignAnnotations(Annotation[] th, Annotation[] tr, String algo) {
+        Annotation[] correspTh = new Annotation[th.length];
+
+        if (algo.equals("multiple")) {
+            for (int i=0 ; i<th.length ; i++) {
+                for (int j=0 ; j<tr.length ; j++) {
+                    if (th[i].intersect(tr[j]) == true
+                            && th[i].getType().equals(tr[j].getType())) {
+                        correspTh[i] = tr[j];
+                    }
+                }
+            }
+        }
+
+        else if (algo.equals("greedy")) {
+            // matrix creation
+            float[][] t = new float[th.length][tr.length];
+            for (int i=0 ; i<th.length ; i++) {
+                for (int j=0 ; j<tr.length ; j++) {
+                    t[i][j] = th[i].intersectionPercentage(tr[j]);
+                }
+            }
+
+            float max = 1;
+            int imax = 0, jmax = 0;
+            while (max > 0) {
+                // search for the maximum
+                max = 0;
+                for (int i=0 ; i<t.length ; i++) {
+                    for (int j=0 ; j<t[i].length ; j++) {
+                        if (max < t[i][j] && t[i][j] > 0) {
+                            max = t[i][j];
+                            imax = i;
+                            jmax = j;
+                        }
+                    }
+                }
+
+                // add the match in the matches' table
+                correspTh[imax] = tr[jmax];
+
+                // delete the column and the line corresponding with the added annotation
+                for (int i=0 ; i<t.length ; i++) {
+                    t[i][jmax] = 0;
+                }
+                for (int j=0 ; j<t[0].length ; j++) {
+                    t[imax][j] = 0;
+                }
+            }
+        }
+
+//        else if (algo.equals("maximum matching")) {
+//
+//        }
+
+        return correspTh;
     }
 
 
